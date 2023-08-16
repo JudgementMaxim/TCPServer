abbecfd8ab032fb6cf1c130ee21ae171303354b8
Maxim Skalenko <maxim.saklenko@gmail.com>
2023-08-16 09:20:16 +0200

    16.08 Software Stunde

--- server.cpp
+++ server.cpp
@@ -1,11 +1,16 @@
 #include "server.h"
 
+
 server::server(QObject *parent) : QObject(parent)
 {
+
+    createCommands();
+
     tcpServer = new QTcpServer;    //Create Server
-    tcpServer->listen(QHostAddress::Any,414); //Server listens to Port 414 GangGang
+    tcpServer->listen(QHostAddress::Any,414); //Server listens to Port 414local
 
     connect(tcpServer,SIGNAL(newConnection()),this, SLOT(newClientConnection())); //When Client connects to server
+
 }
 
 void server::newClientConnection()
@@ -13,5 +18,40 @@ void server::newClientConnection()
     QTcpSocket *newSocket = tcpServer->nextPendingConnection();
     socket = newSocket;
 
-    socket->write("Moin Mattrose \r\n");
+    socket->write("Connected\r\n");
+    connect(socket,SIGNAL(readyRead()),this,SLOT(readyRead()));
 }
+
+void server::readyRead()
+{
+    while(socket->canReadLine()){
+        QString line = socket->readLine().trimmed(); // Trim leading/trailing whitespace
+        line.toUtf8();
+
+        if(line == "#DATE"){
+            socket->write(&QDate::currentDate().toString().toUtf8());
+        }
+        else if(line == "#DATE"){
+
+        }
+        else if(line == "#DATE"){
+        }
+        else if(line == "#DATE"){
+
+        }
+        else if(line == "#DATE"){
+
+        }
+        else if(line == "#DATE"){
+
+        }else{
+
+        }
+
+        qDebug() << "Received: " << line;
+    }
+}
+
+
+
+
--- server.h
+++ server.h
@@ -10,15 +10,23 @@ class server : public QObject
     Q_OBJECT
 private:
     QTcpServer  *tcpServer = nullptr;  //Server
-    QTcpSocket  *socket = nullptr;  //Addresse
+    QTcpSocket  *socket = nullptr;  //Addres
+    QVector< QVector<QString> > *commands();
+
+    void createCommands();
+
+
+
 public:
     explicit server(QObject *parent = nullptr);
+
+
 
 signals:
 
 public slots:
     void newClientConnection();
-
+    void readyRead();
 };
 
 #endif // SERVER_H
